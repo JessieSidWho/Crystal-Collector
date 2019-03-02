94fad2e61ea18f7f305c61eef13e5c445f6f5699
Jessie Sidhu <jsidhu22@gmail.com>
2019-03-01 18:59:50 -0800

    js file started, no css file yet

---
+++ assets/game.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+
+    $(".btn").on("click",function(event){
+
+        // console.log(this.getAttribute("data-id"))
+        // console.log($(this).attr("data-id"))
+
+
+    });
+    $("#targetNum").html("Target: ***");
+    $("#totalNum").html("Total: ***");
+    $("#wins").html("Wins: ***");
+    $("#losses").html("Losses: ***")
+
+});
